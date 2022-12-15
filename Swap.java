c50252e33b69e56c2b7a962edbad3217c6d534cb
Abinaya37 <abinaya30091998@gmail.com>
2022-12-15 12:36:32 +0530

    file added

---
+++ Swap.java
@@ -0,0 +1,32 @@
+package com.programs;
+
+public class Swap {
+
+	public static void main(String[] args) {
+
+		int a = 10;
+		int b = 20;
+
+		System.out.println("Before swapping values are " + a + " " + b);
+
+		// 1st type
+
+		int t = a; // t=10
+		a = b; // a=b ---> a=20
+		b = t; // b=t ---> b=10
+
+		System.out.println("After swapping values are " + a + " " + b);
+
+		// 2nd type
+
+		int i = 10, j = 20;
+
+		i = i + j;  //30
+		j = i - j;  //30-20-->10
+		i = i - j;  //30-10 -->20
+
+		System.out.println("After swapping values using 2nd type are " + a + " " + b);
+
+	}
+
+}
